9d2c8002a1659a5850ac4a52c08fdb7bed95ea95
carvals <gourouotahy@gmail.com>
2018-12-28 10:18:10 +0100

    4 items added

---
+++ lib/ui/select_item.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+
+class SelectItemForm extends StatefulWidget {
+  @override
+  _SelectItemFormState createState() => _SelectItemFormState();
+}
+
+class _SelectItemFormState extends State<SelectItemForm> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: new AppBar(
+        title: new Text('Select Items'),
+      ),
+      body: new Container(
+        padding: EdgeInsets.symmetric(horizontal:8.0, vertical:20.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          padding: EdgeInsets.all(3.0),
+          children: <Widget>[
+            makeDashboardItem('Event', Icons.new_releases,CardType.Event),
+            makeDashboardItem('Shop', Icons.store, CardType.Shop),
+            makeDashboardItem('Product', Icons.queue, CardType.Product),
+            makeDashboardItem('Services', Icons.room_service, CardType.Services),
+          ],
+        ),
+      ),
+    );
+  }
+
+//new method here
+
+
+
+Card makeDashboardItem(String title, IconData icon, CardType cardType) {
+    
+    return Card(
+        elevation: 1.0,
+        margin: new EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(color: Color.fromRGBO(220, 220, 220, 1.0)),
+          child: new InkWell(
+            onTap: ()  {
+                switch(cardType)
+                {
+                  case CardType.Event:
+                  debugPrint("event");
+                  break;
+
+                  case CardType.Product:
+                  debugPrint("product");
+                  break;
+
+                  case CardType.Services:
+                  debugPrint("services");
+                  break;
+                  
+                  case CardType.Shop:
+                  debugPrint("shop");
+                  break;
+
+                }
+            },
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.min,
+              verticalDirection: VerticalDirection.down,
+              children: <Widget>[
+                SizedBox(height: 50.0),
+                Center(
+                    child: Icon(
+                  icon,
+                  size: 40.0,
+                  color: Colors.black,
+                )),
+                SizedBox(height: 20.0),
+                new Center(
+                  child: new Text(title,
+                      style:
+                          new TextStyle(fontSize: 18.0, color: Colors.black)),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+
+  
+}
+
+enum CardType{
+  Event,
+  Product,
+  Services,
+  Shop
+
+}
