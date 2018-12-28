29a5e50f172537a9eb846c0769c06d58bf1a58cd
carvals <gourouotahy@gmail.com>
2018-12-29 00:20:20 +0100

    test cam

--- lib/ui/add_shop.dart
+++ lib/ui/add_shop.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
 
 class AddShop extends StatefulWidget {
   String store;
@@ -10,6 +13,18 @@ class AddShop extends StatefulWidget {
 }
 
 class _AddShopState extends State<AddShop> {
+File _image;
+
+  Future getImage() async {
+    var image = await ImagePicker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = image;
+    });
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,10 +34,22 @@ class _AddShopState extends State<AddShop> {
       body: new Column(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          
+          new Padding(padding: EdgeInsets.all(6.0),),
+          new Container(
+            height: 200,
+            child:new Center(
+        
+        child: _image == null
+            ? new Text('Please take a picture')
+            : new Image.file(_image),
+      ),),
         ],
       ),
-      
+      floatingActionButton: new FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: new Icon(Icons.add_a_photo),
+      ),
     );
   }
 }
